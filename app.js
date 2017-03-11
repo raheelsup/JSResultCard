0dba0f22ad535ef77a6b77f28ef4e23d0b1e5200
raheelsup <raheel.sup@gmail.com>
2017-03-11 11:17:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var a = prompt("Enter Your Percentage");
+
+if (a > 70) {
+    alert("You grade is A")
+}
+else if (a > 60) {
+    alert("Your grade is B")
+}
+else if (a > 49) {
+    alert("Your grade is C")
+}
+else {
+    alert("Sorry, You are fail")
+};
